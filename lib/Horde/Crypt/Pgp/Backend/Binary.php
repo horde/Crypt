379d26ed6f754d6ef3d77dde8cadefb97f2b193a
duritong <peter.meier+github@immerda.ch>
2026-04-02 11:01:23 +0200

    support parsing public keys with just an email (#9)
    Various modern clients start creating keys with just the email-
    address as the string in the uid packet.
    
    If we do not find an RFC-2822 style mail name-addr, we assume the
    user id is just the emailaddress and use it as name and email.

--- lib/Horde/Crypt/Pgp/Backend/Binary.php
+++ lib/Horde/Crypt/Pgp/Backend/Binary.php
@@ -235,7 +235,7 @@ class Horde_Crypt_Pgp_Backend_Binary extends Horde_Crypt_Pgp_Backend
                 if (strpos($lowerLine, ':user id packet:') !== false) {
                     $uid_idx++;
                     $line = preg_replace_callback('/\\\\x([0-9a-f]{2})/', $packetInfoHelper, $line);
-                    if (!preg_match('/"([^\<]+)\<([^\>]+)\>"/', $line, $matches)) {
+                    if (!preg_match('/"([^\<]+)\<([^\>]+)\>"/', $line, $matches) && !preg_match('/"([^\<]+@[^\>]+)"/', $line, $matches)) {
                         continue;
                     }
                     $header = 'id' . $uid_idx;
@@ -259,7 +259,11 @@ class Horde_Crypt_Pgp_Backend_Binary extends Horde_Crypt_Pgp_Backend
                             $keyid = substr(str_replace(' ', '', $m[1]), -16);
                         }
                     }
-                    $out[$key_idx]['signature'][$header]['email'] = $matches[2];
+                    if (array_key_exists(2,$matches)) {
+                      $out[$key_idx]['signature'][$header]['email'] = $matches[2];
+                    } else {
+                      $out[$key_idx]['signature'][$header]['email'] = $matches[1];
+                    }
                     $out[$key_idx]['signature'][$header]['keyid'] = $keyid;
                     continue;
                 }
